bb9f6cd5efa063c9e95e05c05bdb161d2c5a2178
Ayush <ayushsri1810@gmail.com>
2023-08-04 01:42:05 +0530

    se

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,22 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 function ExpenseItems(props){
     // const expenseDate=new Date(2023,8,3);
     // const expenseTitle="car insurance";
     // const expenseAmount=300;
 //NOTE: we have remove hardcode data and make it very easy , now we will see how could we see that things again and again ? the Answer is using "Props {properties}"
+  
     return (
     <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+       <ExpenseDate date={props.date}/>
+        
+   
         <div className = "expense-item__description" >
-        <h2>{props.title}</h2>
-        </div>
-     <div className = "expense-item__price">${props.amount}</div>
-    </div>);
+            <h2>{props.title}</h2>
+       
+            <div className = "expense-item__price">${props.amount}</div>
+        </div> 
+    </div>
+    );
 }
 export default ExpenseItems
