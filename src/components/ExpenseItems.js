77afe0f97ad96ef4ae1f0715fad61043bff01cd7
Ayush <ayushsri1810@gmail.com>
2023-08-03 23:24:33 +0530

    t

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,4 +1,15 @@
+import "./ExpenseItems.css";
 function ExpenseItems(){
-    return <h2>ExpenseItems !</h2>
+    // const expensedate=new Date(2023,8,3);
+    // const expenseitem="car insurance";
+    // const expenseprice=300;
+    return (
+    <div className="expense-item">
+    <div>August 3rd 2023</div>
+        <div className = "expense-item__description" >
+        <h2>car insurance</h2>
+        </div>
+     <div className = "expense-item__price">$300</div>
+    </div>);
 }
 export default ExpenseItems
