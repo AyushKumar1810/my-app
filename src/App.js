63cb17ec24212f7e7b06ce746a3d5d4db47cd391
Ayush <ayushsri1810@gmail.com>
2023-08-04 00:49:41 +0530

    fi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import ExpenseItems from "./components/ExpenseItems";
-function App(){
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+
+    { id: 'e2',
+     title: 'New TV',
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Ayush ! let's get started </h2>
-      <ExpenseItems></ExpenseItems>
+      <h2>Let's get started!</h2>
+     
+   
+      <ExpenseItems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItems
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
-export default App
+
+
+export default App;
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,16 @@
 import "./ExpenseItems.css";
-function ExpenseItems(){
-    const expenseDate=new Date(2023,8,3);
-    const expenseTitle="car insurance";
-    const expenseAmount=300;
-//NOTE: we have remove hardcode data and make it very easy , now we will see how could we sue that things again and again 
+function ExpenseItems(props){
+    // const expenseDate=new Date(2023,8,3);
+    // const expenseTitle="car insurance";
+    // const expenseAmount=300;
+//NOTE: we have remove hardcode data and make it very easy , now we will see how could we see that things again and again ? the Answer is using "Props {properties}"
     return (
     <div className="expense-item">
-    <div>{expenseDate.toISOString()}</div>
+    <div>{props.date.toISOString()}</div>
         <div className = "expense-item__description" >
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         </div>
-     <div className = "expense-item__price">${expenseAmount}</div>
+     <div className = "expense-item__price">${props.amount}</div>
     </div>);
 }
 export default ExpenseItems
