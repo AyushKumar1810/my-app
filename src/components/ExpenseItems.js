20a0fd12f951f11230988cb38964f7d1011188a5
Ayush <ayushsri1810@gmail.com>
2023-08-03 23:33:44 +0530

    fo

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,15 +1,16 @@
 import "./ExpenseItems.css";
 function ExpenseItems(){
-    // const expensedate=new Date(2023,8,3);
-    // const expenseitem="car insurance";
-    // const expenseprice=300;
+    const expenseDate=new Date(2023,8,3);
+    const expenseTitle="car insurance";
+    const expenseAmount=300;
+//NOTE: we have remove hardcode data and make it very easy , now we will see how could we sue that things again and again 
     return (
     <div className="expense-item">
-    <div>August 3rd 2023</div>
+    <div>{expenseDate.toISOString()}</div>
         <div className = "expense-item__description" >
-        <h2>car insurance</h2>
+        <h2>{expenseTitle}</h2>
         </div>
-     <div className = "expense-item__price">$300</div>
+     <div className = "expense-item__price">${expenseAmount}</div>
     </div>);
 }
 export default ExpenseItems
