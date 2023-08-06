4350b4771f287427adddddf0a1cd81e8fa54c9f3
Ayush <ayushsri1810@gmail.com>
2023-08-06 17:49:44 +0530

    se

---
+++ src/components/Newexpense/NewExpense.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NewExpense.css';
+const NewExpense =() =>{
+    return <div className='new-expense'>
+        <form></form>
+    </div>
+};
+export default NewExpense
